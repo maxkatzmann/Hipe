7a8ffa552319bff1c56cbdcbeb6ebb08c8fd3faa
Max Katzmann <m.katzmann@me.com>
2017-07-11 10:34:40 +0200

    Added GPL

--- native_coordinates.py
+++ native_coordinates.py
@@ -1,3 +1,21 @@
+# This program visualizes hyperbolic circles using the native representation.
+# Copyright (C) 2017    Maximilian Katzmann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# You can contact the author via email: max.katzmann@gmail.com
+
 import math
 import euclidean_coordinates
 
