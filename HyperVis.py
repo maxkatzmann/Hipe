f53e0233e4f667713b7208088ecff84f6c8bf9d7
Max Katzmann <m.katzmann@me.com>
2017-07-19 16:45:14 +0200

    Added options to write print the drawing as SVG or Ipe.

--- HyperVis.py
+++ HyperVis.py
@@ -130,7 +130,7 @@ def draw_edge_from_coordinate_to_coordinate(canvas, coord1, coord2, color):
                 r = math.acosh((math.cosh(native_point2.r) * math.cosh(partial_distance) - (math.sinh(native_point2.r) * math.sinh(partial_distance) * cos_gamma_2)))
             except (ZeroDivisionError, ValueError):
                 pass
-                gamma_prime = 0.0
+            gamma_prime = 0.0
             try:
                 gamma_prime = math.acos(((math.cosh(r) * math.cosh(native_point2.r)) - math.cosh(partial_distance)) / (math.sinh(r) * math.sinh(native_point2.r)))
             except (ZeroDivisionError, ValueError):
@@ -173,6 +173,315 @@ selected_nodes = []
 is_circle_node = []
 current_circle_size = 5.0
 colors = ["black", "red", "green", "blue", "orange", "magenta"];
+
+def print_circle(center, radius, filled, color):
+    if filled:
+        print("<circle cx=\"" + str(center.x) + "\" cy=\"" + str(center.y) + "\" r=\"" + str(radius) + "\" fill=\"" + str(color) + "\" stroke=\"" + str(color) + "\" stroke-width=\"1\"/>\n")
+    else:
+        print("<circle cx=\"" + str(center.x) + "\" cy=\"" + str(center.y) + "\" r=\"" + str(radius) + "\" fill=\"clear\" stroke=\"" + str(color) + "\" stroke-width=\"1\"/>\n")
+
+def print_ipe_circle(center, radius, filled, color):
+    if filled:
+        print("<path stroke=\"" + str(color) + "\" fill=\"" + str(color) + "\"> " + str(radius) + " 0 0 " + str(radius) + " " + str(center.x) + " " + str(center.y) + " e </path>")
+    else:
+        print("<path stroke=\"" + str(color) + "\"> " + str(radius) + " 0 0 " + str(radius) + " " + str(center.x) + " " + str(center.y) + " e </path>")
+
+def print_line(coord1, coord2, color):
+    print("<line x1=\"" + str(coord1.x) + "\" y1=\"" + str(coord1.y) + "\" x2=\"" + str(coord2.x) + "\" y2=\"" + str(coord2.y) + "\" stroke=\"" + str(color) + "\" stroke-width=\"1.0\" opacity=\"1.0\"/>\n")
+
+def print_ipe_line(coord1, coord2, color):
+    print("<path stroke=\"" + str(color) + "\">\n" +\
+          str(coord1.x) + " " + str(coord1.y) + " m\n" +\
+          str(coord2.x) + " " + str(coord2.y) + " l\n" +\
+          "</path>")
+
+def print_edge(coord1, coord2, color):
+    scale = 50.0
+    render_detail = 100
+    center = euclidean_coordinates.EuclideanCoordinate(\
+        canvas.winfo_width() / 2.0, \
+        canvas.winfo_height() / 2.0)
+
+    if (coord1.x == center.x and coord1.y == center.y) or (coord2.x == center.x and coord2.y == center.y):
+        print_line(coord1, coord2, color)
+    else:
+        relative_point1 = euclidean_coordinates.coordinate_relative_to_coordinate(coord1, center)
+        native_point1 = relative_point1.to_native_coordinate_with_scale(scale)
+        native_point1.phi += math.pi
+
+        relative_point2 = euclidean_coordinates.coordinate_relative_to_coordinate(coord2, center)
+        native_point2 = relative_point2.to_native_coordinate_with_scale(scale)
+        native_point2.phi += math.pi
+
+        angular_distance = native_point2.phi - native_point1.phi
+        if (angular_distance > 0.0 and angular_distance < math.pi) or angular_distance < -math.pi:
+            native_temp = native_point1
+            native_point1 = native_point2
+            native_point2 = native_temp
+
+        distance = native_coordinates.distance_between(native_point1, native_point2)
+
+        cos_gamma_2 = 0.0
+        try: 
+            cos_gamma_2 = (((math.cosh(native_point2.r) * math.cosh(distance)) - math.cosh(native_point1.r)) / (math.sinh(native_point2.r) * math.sinh(distance)))
+        except (ZeroDivisionError, ValueError):
+            pass
+
+        line_points = []
+        for i in range(0, render_detail):
+            partial_distance = distance * (i / float(render_detail))
+            r = 0.0
+            try:
+                r = math.acosh((math.cosh(native_point2.r) * math.cosh(partial_distance) - (math.sinh(native_point2.r) * math.sinh(partial_distance) * cos_gamma_2)))
+            except (ZeroDivisionError, ValueError):
+                pass
+                gamma_prime = 0.0
+            try:
+                gamma_prime = math.acos(((math.cosh(r) * math.cosh(native_point2.r)) - math.cosh(partial_distance)) / (math.sinh(r) * math.sinh(native_point2.r)))
+            except (ZeroDivisionError, ValueError):
+                pass
+
+            phi = native_point2.phi + gamma_prime
+            native_line_point = native_coordinates.PolarCoordinate(r, phi)
+
+            converted_point = native_line_point.to_euclidean_coordinate_with_scale(scale)
+            euclidean_line_point = euclidean_coordinates.coordinate_relative_to_coordinate(center, converted_point)
+            line_points.append(euclidean_line_point)
+
+            if i > 0:
+                print_line(line_points[i - 1], line_points[i], color)
+
+        if (angular_distance > 0.0 and angular_distance < math.pi) or angular_distance < -math.pi:
+            print_line(line_points[render_detail - 1], coord2, color)
+        else:
+            print_line(line_points[render_detail - 1], coord1, color)
+
+def print_ipe_edge(coord1, coord2, color):
+    scale = 50.0
+    render_detail = 100
+    center = euclidean_coordinates.EuclideanCoordinate(\
+        canvas.winfo_width() / 2.0, \
+        canvas.winfo_height() / 2.0)
+
+    if (coord1.x == center.x and coord1.y == center.y) or (coord2.x == center.x and coord2.y == center.y):
+        print_ipe_line(coord1, coord2, color)
+    else:
+        relative_point1 = euclidean_coordinates.coordinate_relative_to_coordinate(coord1, center)
+        native_point1 = relative_point1.to_native_coordinate_with_scale(scale)
+        native_point1.phi += math.pi
+
+        relative_point2 = euclidean_coordinates.coordinate_relative_to_coordinate(coord2, center)
+        native_point2 = relative_point2.to_native_coordinate_with_scale(scale)
+        native_point2.phi += math.pi
+
+        angular_distance = native_point2.phi - native_point1.phi
+        if (angular_distance > 0.0 and angular_distance < math.pi) or angular_distance < -math.pi:
+            native_temp = native_point1
+            native_point1 = native_point2
+            native_point2 = native_temp
+
+        distance = native_coordinates.distance_between(native_point1, native_point2)
+
+        cos_gamma_2 = 0.0
+        try: 
+            cos_gamma_2 = (((math.cosh(native_point2.r) * math.cosh(distance)) - math.cosh(native_point1.r)) / (math.sinh(native_point2.r) * math.sinh(distance)))
+        except (ZeroDivisionError, ValueError):
+            pass
+
+        line_points = []
+        for i in range(0, render_detail):
+            partial_distance = distance * (i / float(render_detail))
+            r = 0.0
+            try:
+                r = math.acosh((math.cosh(native_point2.r) * math.cosh(partial_distance) - (math.sinh(native_point2.r) * math.sinh(partial_distance) * cos_gamma_2)))
+            except (ZeroDivisionError, ValueError):
+                pass
+            gamma_prime = 0.0
+            try:
+                gamma_prime = math.acos(((math.cosh(r) * math.cosh(native_point2.r)) - math.cosh(partial_distance)) / (math.sinh(r) * math.sinh(native_point2.r)))
+            except (ZeroDivisionError, ValueError):
+                pass
+
+            phi = native_point2.phi + gamma_prime
+            native_line_point = native_coordinates.PolarCoordinate(r, phi)
+
+            converted_point = native_line_point.to_euclidean_coordinate_with_scale(scale)
+            euclidean_line_point = euclidean_coordinates.coordinate_relative_to_coordinate(center, converted_point)
+            line_points.append(euclidean_line_point)
+
+            if i > 0:
+                print_ipe_line(line_points[i - 1], line_points[i], color)
+
+        if (angular_distance > 0.0 and angular_distance < math.pi) or angular_distance < -math.pi:
+            print_ipe_line(line_points[render_detail - 1], coord2, color)
+        else:
+            print_ipe_line(line_points[render_detail - 1], coord1, color)
+
+def print_ipe():
+    global points
+    global edges
+    global circle_sizes
+    global circle_colors
+    global selected_nodes
+    global is_circle_node
+
+    print("<?xml version=\"1.0\"?>\n" +\
+          "<!DOCTYPE ipe SYSTEM \"ipe.dtd\">\n" +\
+          "<ipe version=\"70206\" creator=\"Ipe 7.2.7\">\n" +\
+          "<info created=\"D:20170719160807\" modified=\"D:20170719160807\"/>\n" +\
+          "<ipestyle name=\"basic\">\n" +\
+          "</ipestyle>\n" +\
+          "<page>\n" +\
+          "<layer name=\"alpha\"/>\n" +\
+          "<view layers=\"alpha\" active=\"alpha\"/>\n")
+
+    center = euclidean_coordinates.EuclideanCoordinate(\
+        canvas.winfo_width() / 2.0, \
+        canvas.winfo_height() / 2.0)
+
+    print_ipe_circle(center, 2.0, True, "black")
+
+    scale = 50.0
+    render_detail = 360
+    render_detail_half = render_detail / 2
+
+    for index, point in enumerate(points):
+        if is_circle_node[index]:
+            if index in selected_nodes:
+                print_ipe_circle(point, 2.0, True, "black")
+            else:
+                print_ipe_circle(point, 2.0, True, "black")
+
+            circle_size = circle_sizes[index]
+            relative_point = euclidean_coordinates.coordinate_relative_to_coordinate(point, center)
+            native_point = relative_point.to_native_coordinate_with_scale(scale)
+            native_point.phi += math.pi
+
+            additional_render_detail = 8 * math.floor(native_point.r)
+            angular_point_distance = 2.0 * math.pi / render_detail
+
+            circle_points = []
+            for i in range(render_detail):
+                native_circle_point = native_coordinates.PolarCoordinate(circle_size, i * angular_point_distance)
+                circle_points.append(native_circle_point)
+
+                if abs(i - render_detail_half) < (additional_render_detail / 2):
+                    for j in range(additional_render_detail):
+                        native_circle_point = native_coordinates.PolarCoordinate(circle_size, \
+                        i * angular_point_distance + j * (angular_point_distance / additional_render_detail))
+                        circle_points.append(native_circle_point)
+
+            for i in range(len(circle_points)):
+                native_circle_point = circle_points[i]
+                translated_point = native_coordinates.coordinate_translated_along_x_axis_by_hyperbolic_distance(native_circle_point, native_point.r)
+                rotated_point = native_coordinates.coordinate_rotated_around_origin_by_angle(translated_point, native_point.phi)
+                converted_point = rotated_point.to_euclidean_coordinate_with_scale(scale)
+                euclidean_circle_point = euclidean_coordinates.coordinate_relative_to_coordinate(center, converted_point)
+
+                circle_points[i] = euclidean_circle_point
+
+                if i > 0:
+                    print_ipe_line(circle_points[i - 1], circle_points[i], colors[circle_colors[index]])
+
+            print_ipe_line(circle_points[len(circle_points) - 1], circle_points[0], colors[circle_colors[index]])
+        else:
+            relative_point = euclidean_coordinates.coordinate_relative_to_coordinate(point, center)
+            native_point = relative_point.to_native_coordinate_with_scale(scale)
+            native_point.phi += math.pi
+
+            if index in selected_nodes:
+                print_ipe_circle(point, 2.0, True, "black")
+            else:
+                print_ipe_circle(point, 2.0, True, "black")
+
+    for edge in edges:
+        (node1, node2) = edge
+        print_ipe_edge(points[node1], points[node2], "black")
+
+    print("</page>\n" +\
+          "</ipe>")
+
+def print_svg():
+    global points
+    global edges
+    global circle_sizes
+    global circle_colors
+    global selected_nodes
+    global is_circle_node
+
+    print("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE svg PUBLIC " +\
+          "\"-//W3C//DTD SVG 1.1//EN\" " +\
+          "\"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n\n<svg " +\
+          "xmlns=\"http://www.w3.org/2000/svg\"\nxmlns:xlink=\"http://" +\
+          "www.w3.org/1999/xlink\" " +\
+          "xmlns:ev=\"http://www.w3.org/2001/xml-events\"\nversion=\"1.1\" " +\
+          "baseProfile=\"full\"\nwidth=\"" + str(canvas.winfo_width()) + "\" height=\"" + str(canvas.winfo_height()) + "\">\n\n")
+
+    center = euclidean_coordinates.EuclideanCoordinate(\
+        canvas.winfo_width() / 2.0, \
+        canvas.winfo_height() / 2.0)
+
+    print_circle(center, 2.0, True, "blue")
+
+    scale = 50.0
+    render_detail = 360
+    render_detail_half = render_detail / 2
+
+    for index, point in enumerate(points):
+        if is_circle_node[index]:
+            if index in selected_nodes:
+                print_circle(point, 2.0, True, "red")
+            else:
+                print_circle(point, 2.0, True, "black")
+
+            circle_size = circle_sizes[index]
+            relative_point = euclidean_coordinates.coordinate_relative_to_coordinate(point, center)
+            native_point = relative_point.to_native_coordinate_with_scale(scale)
+            native_point.phi += math.pi
+
+            additional_render_detail = 8 * math.floor(native_point.r)
+            angular_point_distance = 2.0 * math.pi / render_detail
+
+            circle_points = []
+            for i in range(render_detail):
+                native_circle_point = native_coordinates.PolarCoordinate(circle_size, i * angular_point_distance)
+                circle_points.append(native_circle_point)
+
+                if abs(i - render_detail_half) < (additional_render_detail / 2):
+                    for j in range(additional_render_detail):
+                        native_circle_point = native_coordinates.PolarCoordinate(circle_size, \
+                        i * angular_point_distance + j * (angular_point_distance / additional_render_detail))
+                        circle_points.append(native_circle_point)
+
+            for i in range(len(circle_points)):
+                native_circle_point = circle_points[i]
+                translated_point = native_coordinates.coordinate_translated_along_x_axis_by_hyperbolic_distance(native_circle_point, native_point.r)
+                rotated_point = native_coordinates.coordinate_rotated_around_origin_by_angle(translated_point, native_point.phi)
+                converted_point = rotated_point.to_euclidean_coordinate_with_scale(scale)
+                euclidean_circle_point = euclidean_coordinates.coordinate_relative_to_coordinate(center, converted_point)
+
+                circle_points[i] = euclidean_circle_point
+
+                if i > 0:
+                    print_line(circle_points[i - 1], circle_points[i], colors[circle_colors[index]])
+
+            print_line(circle_points[len(circle_points) - 1], circle_points[0], colors[circle_colors[index]])
+        else:
+            relative_point = euclidean_coordinates.coordinate_relative_to_coordinate(point, center)
+            native_point = relative_point.to_native_coordinate_with_scale(scale)
+            native_point.phi += math.pi
+
+            if index in selected_nodes:
+                print_circle(point, 2.0, True, "red")
+            else:
+                print_circle(point, 2.0, True, "black")
+
+    for edge in edges:
+        (node1, node2) = edge
+        print_edge(points[node1], points[node2], "black")
+
+    print("\n</svg>\n")
+
 
 def redraw(canvas):
     global points
@@ -383,6 +692,12 @@ def e_pressed(event):
                 edges.append(edge)
     redraw(canvas)
 
+def p_pressed(event):
+    print_svg()
+
+def i_pressed(event):
+    print_ipe()
+
 canvas.bind("<Configure>", resize)
 canvas.bind("<Button-1>", mouse_pressed)
 canvas.bind("<Shift-Button-1>", shift_mouse_pressed)
@@ -398,6 +713,8 @@ root.bind("<MouseWheel>", mouse_scrolled)
 root.bind("c", c_pressed)
 root.bind("o", o_pressed)
 root.bind("e", e_pressed)
+root.bind("p", p_pressed)
+root.bind("i", i_pressed)
 root.bind("+", mouse_scroll_up)
 root.bind("-", mouse_scroll_down)
 
@@ -410,8 +727,11 @@ message = Label(canvas, text = \
                 "E: Add an edge between all selected nodes \n" +\
                 "Back Space: Delete selected point \n" +\
                 "MouseWheel / '+' / '-': Change Circle Radius \n" +\
-                "C: Cycle through the colors [Black, Green, Red, Blue, Orange and Magenta]", justify=LEFT)
+                "C: Cycle through the colors [Black, Green, Red, Blue, Orange and Magenta]\n" +\
+                "S: Print the drawing as SVG\n" +\
+                "I: Print the drawing as IPE", justify=LEFT)
 message.pack(side = BOTTOM, anchor=W)
+
 
 while True:
     try:
--- README.md
+++ README.md
@@ -12,6 +12,8 @@ native representation.
 
 # Usage
 Launch the tool by calling `python3 HyperVis.py`
+If you plan on saving your drawing as SVG or Ipe, use the `>` operator to write into a file, e.g.:
+`python3 HyperVis.py > drawing.svg` or `python3 HyperVis.py > graphic.ipe`
 
 # Controls
 * `Right Click` adds a circle centered at the current location of the mouse
@@ -24,6 +26,8 @@ Launch the tool by calling `python3 HyperVis.py`
 * `O` adds a circle centered at the origin
 * `E` adds an edge between all selected points
 * `C` cycles through the colors of the selected circle `[Black, Green, Red, Blue, Orange and Magenta]`
+* `S` prints the drawing as SVG (use `>` operator when launching HyperVis to write into a file)
+* `I` prints the drawing as Ipe (use `>` operator when launching HyperVis to write into a file)
 
 # Notes
 * The blue point at the center represents the origin of the hyperbolic plane
